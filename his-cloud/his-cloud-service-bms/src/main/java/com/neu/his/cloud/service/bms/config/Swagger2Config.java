870c91456bcd2551f89997db4880a31c35e6acc8
zain <994130442@qq.com>
2019-07-13 00:36:38 +0800

    update readme

--- his-cloud/his-cloud-service-bms/src/main/java/com/neu/his/cloud/service/bms/config/Swagger2Config.java
+++ his-cloud/his-cloud-service-bms/src/main/java/com/neu/his/cloud/service/bms/config/Swagger2Config.java
@@ -55,9 +55,6 @@ public class Swagger2Config {
     private List<SecurityContext> securityContexts() {
         //设置需要登录认证的路径
         List<SecurityContext> result = new ArrayList<>();
-        result.add(getContextByPath("/brand/.*"));
-        result.add(getContextByPath("/product/.*"));
-        result.add(getContextByPath("/productCategory/.*"));
         return result;
     }
 
